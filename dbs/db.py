6e7af9ceee80a0b4ba5224b614d99de31e2f00b6
Tina Topka <anon.justch1ll1n@gmail.com>
2025-05-07 01:07:14 +0300

    db.py is safe to commit now yay

---
+++ dbs/db.py
@@ -0,0 +1,189 @@
+import pyodbc as odbc
+from typing import List
+import datetime
+from models.user_model import User
+from models.problem_model import Problem
+from models.message_model import Message
+import os
+
+DRIVER_NAME = os.environ['DRIVER_NAME']
+SERVER_NAME = os.environ['SERVER_NAME']
+DATABASE_NAME = os.environ['DATABASE_NAME']
+
+# uid=<username>;
+# pwd=<password>;
+connection_string = f"""
+    DRIVER={{{DRIVER_NAME}}};
+    SERVER={SERVER_NAME};
+    DATABASE={DATABASE_NAME};
+    Trust_Connection=yes;
+"""
+
+conn = odbc.connect(driver=DRIVER_NAME, server=SERVER_NAME, database=DATABASE_NAME)
+# conn.setdecoding(odbc.SQL_CHAR, encoding='UTF-16')
+# conn.setencoding('UTF-16')
+c = conn.cursor()
+
+
+# User
+def find_user_id(id: int):
+    c.execute('SELECT Id, UserName, UserRole FROM cUser WHERE Id = ?', (id,))
+
+    row = c.fetchone()
+    return row
+
+
+def find_user(name: str):
+    c.execute('SELECT Id, UserName, UserRole FROM cUser WHERE UserName = ?', (name,))
+
+    row = c.fetchone()
+    return row
+
+
+def insert_user(user: User):
+    c.execute('SELECT COUNT(*) FROM cUser')
+    count = c.fetchone()[0]
+    user.id = count+1 if count else 0
+
+    with conn:
+        c.execute('INSERT INTO cUser VALUES (?, ?, ?)',
+                  (user.id, user.name, user.role))
+        conn.commit()
+
+
+def delete_user_db(id):
+    c.execute('SELECT COUNT(*) FROM cUser')
+    count = c.fetchone()[0]
+
+    with conn:
+        c.execute('DELETE FROM cUser WHERE Id=?', (id))
+        for i in range(id+1, count):
+            change_position('cUser', i, i-1, False)
+        conn.commit()
+
+
+def update_user(id, name, role):
+    with conn:
+        if name is not None and role is not None:
+            c.execute('UPDATE cUser SET UserName=?, UserRole=? WHERE Id=?',
+                      (name, role, id))
+        elif name is not None:
+            c.execute('UPDATE cUser SET UserName=? WHERE Id=?',
+                      (name, id))
+        elif role is not None:
+            c.execute('UPDATE cUser SET UserRole=? WHERE Id=?',
+                      (role, id))
+        conn.commit()
+
+
+def get_all_users() -> List[User]:
+    c.execute('SELECT * FROM cUser')
+    res = c.fetchall()
+    users = []
+    for i in res:
+        users.append(User(*i))
+    return users
+
+
+# Problem
+def find_problem(title: str):
+    c.execute('SELECT Id, Title, Progress FROM cProblem WHERE Title = ?', (title,))
+
+    row = c.fetchone()
+    return row
+
+
+def get_all_problems() -> List[Problem]:
+    c.execute('SELECT * FROM cProblem')
+    res = c.fetchall()
+    problems = []
+    for i in res:
+        problems.append(Problem(*i))
+    return problems
+
+
+def insert_problem(problem: Problem):
+    c.execute('SELECT COUNT(*) FROM cProblem')
+    count = c.fetchone()[0]
+    problem.id = count+1 if count else 0
+
+    with conn:
+        c.execute('INSERT INTO cProblem VALUES (?, ?, ?)',
+                  (problem.id, problem.title, problem.progress))
+        conn.commit()
+
+
+def delete_problem_db(id):
+    c.execute('SELECT COUNT(*) FROM cProblem')
+    count = c.fetchone()[0]
+
+    with conn:
+        c.execute('DELETE FROM cProblem WHERE Id=?', (id))
+        for i in range(id+1, count):
+            change_position('cProblem', i, i-1, False)
+        conn.commit()
+
+
+def update_problem(id, title, progress):
+    with conn:
+        if title is not None and progress is not None:
+            c.execute('UPDATE cProblem SET Title=?, Progress=? WHERE Id=?',
+                      (title, progress, id))
+        elif title is not None:
+            c.execute('UPDATE cProblem SET Title=? WHERE Id=?',
+                      (title, id))
+        elif progress is not None:
+            c.execute('UPDATE cProblem SET Progress=? WHERE Id=?',
+                      (progress, id))
+        conn.commit()
+
+
+# Message
+def send_message(message: Message):
+    c.execute('SELECT COUNT(*) FROM cMessage')
+    count = c.fetchone()[0]
+    message.id = count+1 if count else 0
+
+    with conn:
+        c.execute('INSERT INTO cMessage VALUES (?, ?, ?, ?, ?, ?, ?)',
+                  (message.id, message.descript, message.mssg_type,
+                   message.mssg_date, message.sender, message.receiver, message.problem))
+        conn.commit()
+
+
+def get_all_messages_sent(id: int) -> List[Message]:
+    c.execute("""SELECT cMessage.Id, cMessage.Descript, cMessage.MssgType, cMessage.MssgDate, cMessage.Sender, cUser.UserName, cProblem.Title
+                        FROM cMessage INNER JOIN cProblem ON cMessage.Problem=cProblem.Id
+                        INNER JOIN cUser ON cMessage.Receiver=cUser.Id
+                        WHERE Sender=?
+                        ORDER BY MssgDate DESC""",
+              (id,))
+    all_mess = c.fetchall()
+    messages = []
+    for i in all_mess:
+        messages.append(Message(*i))
+
+    return messages
+
+
+def get_all_messages_received(id: int) -> List[Message]:
+    c.execute("""SELECT cMessage.Id, cMessage.Descript, cMessage.MssgType, cMessage.MssgDate, cUser.UserName, cMessage.Receiver, cProblem.Title
+                        FROM cMessage INNER JOIN cProblem ON cMessage.Problem=cProblem.Id
+                        INNER JOIN cUser ON cMessage.Sender=cUser.Id
+                        WHERE Receiver=?
+                        ORDER BY MssgDate DESC""",
+              (id,))
+    all_mess = c.fetchall()
+    messages = []
+    for i in all_mess:
+        messages.append(Message(*i))
+
+    return messages
+
+
+def change_position(table_name: str, last_id: int, new_id: int, commit=True):
+    c.execute('UPDATE :table_name SET Id=:new_id WHERE Id= :last_id',
+              {'table_name': table_name, 'new_id': new_id, 'last_id': last_id})
+
+    if commit:
+        conn.commit()
